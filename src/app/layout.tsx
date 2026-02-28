44a0f8332893bce067973ca67be0ea5f3afd42eb
Veselin Georgiev Vachkov <vesko@Veselins-MacBook-Air.local>
2026-03-01 00:27:18 +0200

    Wrap Header in Suspense to fix useSearchParams prerender error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Suspense } from "react";
 import "./globals.css";
 import { Header } from "@/components/layout/Header";
 import { Footer } from "@/components/layout/Footer";
@@ -31,7 +32,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
       >
-        <Header />
+        <Suspense>
+          <Header />
+        </Suspense>
         <div className="flex-1">{children}</div>
         <Footer />
         <Toaster />
